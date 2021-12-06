46580429b1fec59de8211e7fee552e92aca3f3bf
Nicholas Vlisidis <nvlisidis1993@gmail.com>
2021-12-06 00:20:59 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Nicholas Vlisidis on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
